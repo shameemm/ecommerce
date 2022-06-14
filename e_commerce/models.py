dd7df76b4522aed22c35bab0655ec76775632fe9
shameemm <mandayappuramshameem@gmail.com>
2022-06-14 21:35:45 +0530

    update in models

--- e_commerce/models.py
+++ e_commerce/models.py
@@ -8,6 +8,6 @@ class Categories(models.Model):
 class Products(models.Model):
     pname = models.CharField(max_length=100)
     description = models.CharField(max_length=200)
-    image = models.ImageField(upload_to='images')
+    image = models.ImageField(upload_to='images', default='image/1.jpg', blank=True)
     price = models.IntegerField()
-    category = models.CharField(max_length=100)
+    category = models.ForeignKey(Categories, on_delete=models.SET_NULL, null=True, blank=True)
